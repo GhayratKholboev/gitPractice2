672c4f60992ae464e00a1e1c55af399de09c54ce
GhayratKholboev <64181246+GhayratKholboev@users.noreply.github.com>
2020-04-28 16:02:31 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -5,7 +5,8 @@ public class HappyBirthday {
     public static void main(String[] args) {
         
         System.out.println("Happy Birthday B18");
-        System.out.println("Happy Birthday Cybertek!!!")
+        System.out.println("Happy Birthday Cybertek!!!");
+        System.out.println("Hello Ghayrat");
         
         
 
